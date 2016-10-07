e6a9bf1f89f4831a7c48864ddd4a66c3ce0961c5
zhhjemotion <zhhjemotoin@hotmail.com>
2016-10-07 13:07:31 +0200

    opencv_test4

---
+++ opencv_test4/opencv_test4/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  opencv_test4
+//
+//  Created by 张行剑 on 2016/10/7.
+//  Copyright © 2016年 Lukas_Zhang. All rights reserved.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    VideoCapture cap(0);
+    if(!cap.isOpened()){
+        return -1;
+    }
+    Mat frame;
+    
+    while(true){
+        cap>>frame;
+        imshow("当前视频", frame);
+        if(waitKey(30) >= 0) break;
+    }
+    
+    return 0;
+}
+
+
